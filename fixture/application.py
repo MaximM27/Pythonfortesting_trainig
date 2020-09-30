f99ce3480587643b7223b7eee9b0fc722d3d2d19
Maxim <makarovma27@gmail.com>
2020-09-30 15:56:34 +0300

    Два класс-менеджера объединены в один

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.contact import ContactHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -10,25 +11,12 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
+        self.group = GroupHelper(self)
 
     def return_to_home_page(self):
         # retutn to home page
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
-
-    def add_contact(self, Contact):
-        # add contact
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
-        wd.find_element_by_name("submit").click()
-        self.return_to_home_page()
 
     def open_home_page(self):
         # open home page
