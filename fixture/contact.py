cdfbd326654bfb8e714b3111b0583f39f2b64366
Maxim <makarovma27@gmail.com>
2020-10-02 16:51:07 +0300

    Выделены методы по заполнению форм для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,13 +7,7 @@ class ContactHelper:
         # add contact
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
+        self.fill_contact_form(Contact)
         wd.find_element_by_name("submit").click()
         self.app.return_to_home_page()
 
@@ -26,13 +20,7 @@ class ContactHelper:
     def edit_first_contact(self, Contact):
         wd = self.app.wd
         wd.find_element_by_xpath("(//img[@alt='Edit'])").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
+        self.fill_contact_form(Contact)
         wd.find_element_by_name("update").click()
         self.app.return_to_home_page()
 
@@ -40,3 +28,13 @@ class ContactHelper:
         # retutn to home page
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def fill_contact_form(self, Contact):
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
