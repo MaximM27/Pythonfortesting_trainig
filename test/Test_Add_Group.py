442a2cb23850a43b75da262243112e1b101aea5e
Maxim <makarovma27@gmail.com>
2020-10-06 22:14:46 +0300

    Доработан вспомогательный класс Session

--- test/Test_Add_Group.py
+++ test/Test_Add_Group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create_g(Group(name="groupgroup", header="groupgroup", footer="groupgroup"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
