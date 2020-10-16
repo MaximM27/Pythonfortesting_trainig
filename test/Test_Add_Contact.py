042ecdddb4f9ca1549acb82b93b822f17b54d370
Maxim <makarovma27@gmail.com>
2020-10-16 10:48:33 +0300

    Реализованы проверки путем сравнения списков групп

--- test/Test_Add_Contact.py
+++ test/Test_Add_Contact.py
@@ -3,6 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.add_contact(Contact(firstname="First name", middlename="Middle Name", lastname="Last Name"))
 
 
