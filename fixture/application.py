65b91b3c3fc81b6b2669ae0c8ca4d939d1b094f7
Maxim <makarovma27@gmail.com>
2020-10-23 18:50:37 +0300

    Реализованы опции командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,20 @@ from fixture.group import GroupHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
         self.group = GroupHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -29,6 +37,7 @@ class Application:
         # open home page
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
