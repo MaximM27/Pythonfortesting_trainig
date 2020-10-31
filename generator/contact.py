9a76048ab2c1a38e3c45c480c95bb8594da0a06d
Maxim <makarovma27@gmail.com>
2020-10-31 13:14:49 +0300

    Реализована загрузка тестовых данных в файл в формате JSON методом jsonpickle

---
+++ generator/contact.py
@@ -0,0 +1,42 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import getopt
+import sys
+import jsonpickle
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+
+def random_string(maxlen):
+    symbols = string.ascii_letters + " "*5
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+n = 5
+f = "data/contacts.json"
+
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+testdata = [Contact(firstname="", middlename="",
+                    lastname="")] + [Contact(firstname=random_string(5), middlename=random_string(10),
+                    lastname=random_string(10)) for i in range(n)]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", f)
+
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/Test_Add_Contact.py
+++ test/Test_Add_Contact.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(maxlen):
-    symbols = string.ascii_letters + " "*5
-    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", middlename="",
-                    lastname="")] + [Contact(firstname=random_string(5), middlename=random_string(10),
-                    lastname=random_string(10)) for i in range(5)]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.add_contact(contact)
