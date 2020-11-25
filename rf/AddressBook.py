cef3b00f3382b96b0d6a92102fb468dc29332ca9
Maxim <makarovma27@gmail.com>
2020-11-25 13:34:08 +0300

    Реализованы тесты с помощью фреймворка RobotFramework

---
+++ rf/AddressBook.py
@@ -0,0 +1,31 @@
+import json
+import os.path
+from fixture.application import Application
+from fixture.db import DbFixture
+from model.group import Group
+
+
+class Address:
+
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+
+    def __init__(self, config="target.json", browser="firefox"):
+        self.browser = browser
+        config.file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", config)
+        with open(config.file) as f:
+            self.target = json.load(f)
+
+    def init_fixtures(self):
+        web_config = self.target['web']
+        self.fixture = Application(browser=self.browser, base_url=web_config['baseUrl'])
+        db_config = self.target['web']
+        self.dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+
+    def destroy_fixtures(self):
+        self.dbfixture.destroy()
+        self.fixture.destroy()
+
+    def create_group(self, name, header, footer):
+        self.fixture.group.create(Group(name=name, header=header, footer=footer))
+
+
